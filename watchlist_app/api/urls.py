a6f2039a390b3be937025eb95a4f269cf7cc8d24
ganeshkp <ganeshkumar.patil@gmail.com>
2023-09-06 18:57:50 +0100

    added custom Router

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from watchlist_app.api import views
 from rest_framework.routers import DefaultRouter
+from watchlist_app.api.custom_routers import CustomRouter1
 
 router = DefaultRouter()
 # Using ViewSet
@@ -21,6 +22,9 @@ router.register(r"watchlist-viewset4", views.WatchListViewSet4, basename="watchl
 router1 = DefaultRouter()
 router1.register(r"watchlist-viewset5", views.WatchListViewSet5, basename="watchlist-viewset5")
 router1.register(r"streamplatform-viewset5", views.StreamPlatformViewSet5, basename="streamplatform-viewset5")
+
+router2 = CustomRouter1()
+router2.register(r"streamplatform-viewset3-custom", views.StreamPlatformViewSet3, basename="streamplatform_viewset3_custom")
 
 urlpatterns = [
     # Using Basic Serializer for the views
@@ -106,6 +110,10 @@ urlpatterns = [
     #Using HyperlinkedModelSerializer
     path('', include((router1.urls, 'app_name'), namespace='instance_name')),
     
+    # Using CustomRouter1
+    path("", include(router2.urls)),
+    
 ]
 
 # urlpatterns += router.urls
+# urlpatterns += router2.urls
